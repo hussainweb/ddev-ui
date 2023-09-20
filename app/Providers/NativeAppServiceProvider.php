8e929860333650c1eb0c49baf4991dcd3d10847e
hw <hussainweb@gmail.com>
2023-09-19 21:53:19 -0400

    feat: add nativephp with configuration

---
+++ app/Providers/NativeAppServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Native\Laravel\Contracts\ProvidesPhpIni;
+use Native\Laravel\Facades\Window;
+
+class NativeAppServiceProvider implements ProvidesPhpIni
+{
+    /**
+     * Executed once the native application has been booted.
+     * Use this method to open windows, register global shortcuts, etc.
+     */
+    public function boot(): void
+    {
+        Window::open()
+            ->route('filament.dashboard.resources.projects.index')
+            ->width(900)
+            ->height(700)
+            ->minWidth(300)
+            ->minHeight(200)
+            ->showDevTools(false);
+    }
+
+    /**
+     * Return an array of php.ini directives to be set.
+     */
+    public function phpIni(): array
+    {
+        return [
+        ];
+    }
+}
